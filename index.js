eed32c6c6be079e965738a35f19871fa06a4d692
walid885 <bettaieb88walid@gmail.com>
2023-03-05 16:28:18 +0100

    using express to create get in order to get all memebers function

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 const express = require('express');
 const path = require('path')
 
-const app = express();
+const memebres = [
+    {
+        id:1,
+        name:'John Doe',
+        email:'John@gmail.com',
+        status:'active'
 
-app.get('/', (req,res)=> {
-    res.sendFile(path.join(__dirname,'Public','index.html'))
+    },
+    {
+        id:2,
+        name:'John2Doe',
+        email:'John2@gmail.com',
+        status:'active'
+
+    },
+    {
+        id:3,
+        name:'John3Doe',
+        email:'John3@gmail.com',
+        status:'active'
+
+    },
+    {
+        id:4,
+        name:'John4Doe',
+        email:'John4@gmail.com',
+        status:'active'
+
+    },
+    {
+        id:5,
+        name:'John5Doe',
+        email:'John5@gmail.com',
+        status:'active'
+
+    },
+    
+
+]
+const app = express();
+// gets all memebres
+app.get('/api/memebers' , (req,res)=>{
+res.json(memebres)
+
 })
+// set a static user 
+app.use(express.static(path.join(__dirname,'Public')))
 
 const port = process.env.PORT || 5000;
 
