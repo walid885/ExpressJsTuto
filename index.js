d45df1d1b05a0cc2c0def38258a68b240d53be41
walid885 <bettaieb88walid@gmail.com>
2023-03-05 16:06:45 +0100

    linking to a folder

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const path = require('path')
+
 const app = express();
+
+app.get('/', (req,res)=> {
+    res.sendFile(path.join(__dirname,'Public','index.html'))
+})
+
 const port = process.env.PORT || 5000;
+
+
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
